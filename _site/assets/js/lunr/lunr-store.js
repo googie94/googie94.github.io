0b7058636c1860d729bcede0d471c6b8474d5680
googie <googieyu@gmail.com>
2021-06-12 14:59:46 +0900

    add

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Welcome to googie's blog",
-        "excerpt":"Title   hello world!   ","categories": ["jekyll"],
+        "excerpt":"Title   hello world  ","categories": ["jekyll"],
         "tags": [],
         "url": "/jekyll/first/",
         "teaser": null
