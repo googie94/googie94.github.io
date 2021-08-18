7afb2a6ccf9bcfa01ba39d3d8e5d2aa7abb321a5
googie94 <sportfolio94@gmail.com>
2021-08-18 15:59:02 +0900

    asdd

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# googie94.github.io
+# This is my portfolio site based on blog.
+https://jihyeleee.com/
+![image](https://www.jihyeleee.com/img/share-img.jpg)
+It's forked from (chesterhow/tale)[https://github.com/chesterhow/tale].
+
+© Jihye Leee. 2021
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome to googie's blog",
-        "excerpt":"title   first   title2   second   hello world  ","categories": ["first"],
-        "tags": [],
-        "url": "/first/first/",
-        "teaser": null
-      }]
